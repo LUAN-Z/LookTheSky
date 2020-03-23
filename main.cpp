9e4ea832f4b22e684c341056bae9e29624dfb6a5
LUAN-Z <1120876288@qq.com>
2020-03-23 18:23:37 +0800

    main 函数

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿#include "lookthesky.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    a.setFont(QFont(":/assets/fonts/SFD.ttf"));
+    LookTheSky w;
+    w.show();
+    return a.exec();
+}
